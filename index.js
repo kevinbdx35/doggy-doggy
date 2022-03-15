0bf550f74f0e4c894f848a3c647a0d63958003e4
kevinbdx35 <k.bardoux@orange.fr>
2022-03-15 17:19:13 +0100

    feature to open and write the file

--- index.js
+++ index.js
@@ -3,4 +3,19 @@ const superagent = require('superagent');
 
 fs.readFile(`${__dirname}/dog.txt`, (err, dat) => {
   console.log(`Breed : ${data}`);
+
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .then((res) => {
+      if (err) {
+        return console.error(err.message);
+      }
+
+      fs.writeFile('dog-img.txt', res.body.message, (err) => {
+        if (err) {
+          return console.error(err.message);
+        }
+        console.log('Random dog image saved to file!');
+      });
+    })
 });
