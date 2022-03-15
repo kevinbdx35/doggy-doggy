4e248e2ae41f4719a50ae831d351269cc7b19bda
kevinbdx35 <k.bardoux@orange.fr>
2022-03-15 17:24:59 +0100

    fix error

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ fs.readFile(`${__dirname}/dog.txt`, (err, dat) => {
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
     .then((res) => {
+      console.log(res.body.message);
+
       fs.writeFile('dog-img.txt', res.body.message, (err) => {
         if (err) {
           return console.error(err.message);
